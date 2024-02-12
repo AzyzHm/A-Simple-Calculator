168a15a7de16a8101b8fbffb0c6fdf359a1c185c
Mohammed Aziz Hammemi <128410730+AzyzHm@users.noreply.github.com>
2024-02-12 20:04:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+screen = document.querySelector('.screen');
+
+function add(value) {
+  if (screen.innerText === '0') {screen.innerText = value;}
+  else  screen.innerText += value;
+}
+
+function remove() {
+  if (screen.innerText.length === 1) {
+    screen.innerText = '0';
+  } else {
+    screen.innerText = screen.innerText.slice(0, -1);
+  }
+}
+
+function clear() {
+    screen.innerText = '0';
+}
+
+function calculate() {
+    try {
+        screen.innerText = eval(screen.innerText);
+    }catch (e) {
+        screen.innerText = 'Error';
+    }}
+
+  let removeButton = document.querySelector('.remove');
+  let clearButton = document.querySelector('.clear');
+    
+  removeButton.addEventListener('click', remove);
+  clearButton.addEventListener('click', clear);
+    
